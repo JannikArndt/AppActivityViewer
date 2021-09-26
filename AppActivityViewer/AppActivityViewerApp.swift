4a227bbfd49284a92bf22dfa6ad56ba947e31edf
Jannik Arndt <jannik@jannikarndt.de>
2021-09-26 23:06:32 +0200

    Add LifeCycle to set settings

--- AppActivityViewer/AppActivityViewerApp.swift
+++ AppActivityViewer/AppActivityViewerApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct AppActivityViewerApp: App {
+    @Environment(\.scenePhase) var lifeCycle
+
     var body: some Scene {
         DocumentGroup(viewing: AppActivityViewerDocument.self) { file in
             ContentView(document: file.$document)
+        }.onChange(of: lifeCycle) { newLifeCyclePhase in
+            switch newLifeCyclePhase {
+            case .active:
+                print("App is active")
+                if let documentBrowserViewController = UIApplication.shared.windows.first?.rootViewController as? UIDocumentBrowserViewController {
+                    documentBrowserViewController.allowsDocumentCreation = false
+                }
+            case .inactive:
+                print("App is inactive")
+            case .background:
+                print("App is going to the Background")
+            @unknown default:
+                print("default")
+            }
         }
     }
 }
