25a52d71c3aba04a63452a3d084fa552ed042708
Jannik Arndt <jannik@jannikarndt.de>
2021-09-26 15:33:36 +0200

    Initial Commit

---
+++ AppActivityViewer/AppActivityViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppActivityViewerApp.swift
+//  AppActivityViewer
+//
+//  Created by Jannik Arndt on 26.09.21.
+//
+
+import SwiftUI
+
+@main
+struct AppActivityViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppActivityViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppActivityViewer
+//
+//  Created by Jannik Arndt on 26.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
